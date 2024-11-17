e83663e7f9e3592a0a3cd5ebfe076715cb8c862f
Joris <j.hoevenberg@recreate365.com>
2024-11-18 00:26:27 +0100

    restructure MyHomePage build method, centeraxis and add sizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -41,15 +41,18 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: (appState.getNext),
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: (appState.getNext),
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -71,11 +74,15 @@ class BigCard extends StatelessWidget {
     );
     return Card(
       color: theme.colorScheme.primary,
+      elevation: 10,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asPascalCase, style: style),
+        child: Text(
+            pair.asPascalCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
-      elevation: 10,
     );
   }
 }
