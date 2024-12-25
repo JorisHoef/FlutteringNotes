b2480991a604e614e9ad21d0e7d8f4e40eb7959a
Joris <j.hoevenberg@recreate365.com>
2024-12-25 18:48:15 +0100

    CRUD locally in sharedPreferences the notes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'constants/app_strings.dart';
 import 'screens/main_screen.dart';
 import 'states/note_state.dart';
+import 'storage/local_note_repository.dart';
 import 'themes/theme_provider.dart';
 
 void main() {
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => NoteState()),
+        // Initialize NoteState with LocalNoteRepository
+        ChangeNotifierProvider(
+          create: (context) => NoteState(LocalNoteRepository())..loadNotes(),
+        ),
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
       ],
       child: Consumer<ThemeProvider>(
@@ -25,7 +29,7 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: AppStrings.appName,
             theme: themeProvider.themeData,
-            home: MainScreen(),
+            home: MainScreen(), // The main entry-point screen
           );
         },
       ),
--- lib/models/note.dart
+++ lib/models/note.dart
@@ -9,6 +9,25 @@ class Note {
     required this.content,
   });
 
+  // Method to convert a Note object into a JSON object (Map<String, dynamic>)
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+    };
+  }
+
+  // Factory constructor to create a Note object from a JSON object (Map<String, dynamic>)
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+    );
+  }
+
+  // Copying method to update specific fields immutably
   Note copyWith({int? id, String? title, String? content}) {
     return Note(
       id: id ?? this.id,
